0b2c3ba81f865b8638c60c15f490501ec7ec8d62
Leon Weimann <leonweimann@icloud.com>
2024-11-19 18:19:39 +0100

    Created LineFollower.java

--- lejos-code/Main.java
+++ lejos-code/Main.java
@@ -1,12 +1,8 @@
-import Sensors.TouchSensor;
 import lejos.nxt.Button;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("ES GEHT!");
         Button.waitForAnyPress();
-    
-        TouchSensor sensor = new TouchSensor();
-        sensor.touch();
     }
 }
