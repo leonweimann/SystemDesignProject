6017b2e3acedfca709b4169e11591fef994450b7
Leon Weimann <leonweimann@icloud.com>
2024-11-19 21:47:02 +0100

    Cleaned TouchSensor.java

--- lejos-code/Sensors/TouchSensor.java
+++ lejos-code/Sensors/TouchSensor.java
@@ -1,10 +1,5 @@
 package Sensors;
 
-import lejos.nxt.Button;
-
 public class TouchSensor {
-    public void touch() {
-        System.out.println("Touched");
-        Button.waitForAnyPress();
-    }
+
 }
