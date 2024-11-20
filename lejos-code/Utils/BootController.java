5a09f50c042bec8d6394bc1ab258bfcf5fcbace8
Leon Weimann <leonweimann@icloud.com>
2024-11-20 02:15:14 +0100

    Create + Introduce BootController.java

--- lejos-code/Utils/BootController.java
+++ lejos-code/Utils/BootController.java
@@ -1,5 +1,39 @@
 package Utils;
 
+import Exceptions.BootingException;
+import Utils.BootController;
+import lejos.nxt.Button;
+
 public class BootController {
-    
+    public boolean hasBooted = false;
+    protected BootingException bootingException;
+
+    public BootController() {
+    }
+
+    public void boot() throws BootingException {
+        System.out.println("Start roboting .");
+        // Setup phase 1
+
+        // temporarily
+        String check = "no";
+        if ("Sensors are calibrated" == check) {
+            throwBootException(new BootingException("Test"));
+        }
+
+        System.out.println("Start roboting ..");
+        // Setup phase 2
+        System.out.println("Start roboting ...");
+        // Setup phase 3
+        System.out.println("Ready for take off");
+        System.out.println("Press any button to start");
+        hasBooted = true;
+        Button.waitForAnyPress(); // Wait for the user to press any button to start
+        System.out.println("> Started");
+    }
+
+    private void throwBootException(BootingException e) throws BootingException {
+        bootingException = e;
+        throw e;
+    }
 }
