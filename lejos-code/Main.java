5939b35a0ae15e51ba0bd0b000ce97bcc0582948
Leon Weimann <leonweimann@icloud.com>
2024-11-22 00:11:02 +0100

    Add documentation
    Main.java

--- lejos-code/Main.java
+++ lejos-code/Main.java
@@ -10,8 +10,21 @@ import Coordination.TaskCoordinator;
  * @version 1.6
  */
 public class Main {
+    /**
+     * The runtime coordinator responsible for managing the runtime environment.
+     */
     private static RuntimeCoordinator runtime = new RuntimeCoordinator();
 
+    /**
+     * The main entry point for the application.
+     * 
+     * <p>This method initializes the runtime environment and enters a loop where it
+     * continuously executes tasks coordinated by the TaskCoordinator until the runtime
+     * signals to stop. If an exception occurs during execution, it is caught and its
+     * message is printed to the standard output.</p>
+     * 
+     * @param args Command-line arguments passed to the application.
+     */
     public static void main(String[] args) {
         try {
             runtime.boot();
