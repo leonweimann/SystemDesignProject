108d3d5bc9b533e0c7175b714e2532447b219e55
Leon Weimann <leonweimann@icloud.com>
2024-11-22 00:15:03 +0100

    Add todos
    Main.java

--- lejos-code/Main.java
+++ lejos-code/Main.java
@@ -13,15 +13,19 @@ public class Main {
     /**
      * The runtime coordinator responsible for managing the runtime environment.
      */
-    private static RuntimeCoordinator runtime = new RuntimeCoordinator();
+    private static RuntimeCoordinator runtime = new RuntimeCoordinator(); // TODOD: Maybe Singleton better, since internal phase is needed anywhere ...
 
     /**
      * The main entry point for the application.
      * 
-     * <p>This method initializes the runtime environment and enters a loop where it
-     * continuously executes tasks coordinated by the TaskCoordinator until the runtime
-     * signals to stop. If an exception occurs during execution, it is caught and its
-     * message is printed to the standard output.</p>
+     * <p>
+     * This method initializes the runtime environment and enters a loop where it
+     * continuously executes tasks coordinated by the TaskCoordinator until the
+     * runtime
+     * signals to stop. If an exception occurs during execution, it is caught and
+     * its
+     * message is printed to the standard output.
+     * </p>
      * 
      * @param args Command-line arguments passed to the application.
      */
@@ -33,6 +37,9 @@ public class Main {
                 TaskCoordinator.execute();
             }
         } catch (Exception e) {
+            // TODO: Add handler to RuntimeCoordinator -> Thrown Exceptions from excecution
+            // must be handled as well there, so may rething integration of runtime
+            // internally.
             System.out.println(e.getMessage());
         }
     }
