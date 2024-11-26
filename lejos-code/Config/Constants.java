cc529aa1b5efa3baf548ecbeb6efee2f93b46d25
Leon Weimann <leonweimann@icloud.com>
2024-11-27 00:49:40 +0100

    Deleted Constants.java since unnecessary

--- lejos-code/Config/Constants.java
+++
@@ -1,9 +0,0 @@
-package Config;
-
-public final class Constants {
-    // Private constructor to prevent instantiation
-    private Constants() {
-    }
-
-    public static final int DEFAULT_THRESHOLD = 20;
-}
