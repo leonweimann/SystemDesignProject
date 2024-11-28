3ea0a57d849e3d28d29694a1f88bb292a221db4a
Leon Weimann <leonweimann@icloud.com>
2024-11-28 14:50:42 +0100

    Introduced Direction enum

---
+++ lejos-code/Models/Direction.java
@@ -0,0 +1,18 @@
+package Models;
+
+/**
+ * The {@code Direction} enum represents the possible directions that can be taken.
+ * It includes the following directions:
+ * <ul>
+ *   <li>{@link #STRAIGHT} - Represents moving straight ahead.</li>
+ *   <li>{@link #LEFT} - Represents turning left.</li>
+ *   <li>{@link #RIGHT} - Represents turning right.</li>
+ *   <li>{@link #BACK} - Represents turning back or making a U-turn.</li>
+ * </ul>
+ */
+public enum Direction {
+    STRAIGHT,
+    LEFT,
+    RIGHT,
+    BACK
+}
